77bf2b527d07120d905b767b555f0875f8aebb0d
VictorParraCant <victor.parracant@gmail.com>
2021-03-27 11:28:30 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Wellcome to FEMA.
+        </p>
+        <p>Finance Expenses Management App</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
